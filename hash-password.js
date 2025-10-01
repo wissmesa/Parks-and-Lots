8a00bbf97c2bd5973858d21d9cfad8beee68d2f3
Alem Ruiz <alemrv@gmail.com>
2025-10-01 14:20:11 -0400

    Deleted unnecessary files

--- hash-password.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function hashPassword() {
-  const password = 'password';
-  const hash = await bcrypt.hash(password, 12);
-  console.log('Hashed password:', hash);
-}
-
-hashPassword();
